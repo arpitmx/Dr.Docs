7811f41bf8ab77795614be1c40edefa973a22e15
UtsavUpadhyay08 <utsavup2004@gmail.com>
2022-10-01 18:58:17 +0530

    kuch bhi

---
+++ src/routers/patient.js
@@ -0,0 +1,26 @@
+const express=require('express')
+const router = new express.Router()
+
+const Patient=require('../models/patients')
+
+router.post('/patient',async(req,res)=>{
+    const patient=new Patient(req.body)
+    try{
+        await patient.save()
+        res.status(201).send(patient)
+    }
+    catch(e){
+        console.log(e);
+    }
+})
+
+
+
+/*
+
+Display data
+SignUp
+LogIn
+Fetch File
+
+*/ 
